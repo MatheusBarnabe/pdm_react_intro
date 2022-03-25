16892b6b9f4bd7f2dbd28013a97606abf5adbecb
MatheusBarnabe <matheusbp88@gmail.com>
2022-03-25 17:50:16 -0300

    Terminamos a apostila 03, criando um componente React e estudando sobre JSX

--- src/index.js
+++ src/index.js
@@ -1,8 +1,30 @@
 import React from 'react'
 import ReactDOM from 'react-dom' 
+import './styles.css' 
 
-const App = () => {     
-    return <div>Meu primeiro componente ReactJS</div>
+const App = () => {   
+    const estilosBotao = {
+        marginTop: 12,
+        paddingTop: 8, 
+        paddingBottom: 8, 
+        backgroundColor: 'blueviolet',
+        color: 'white',
+        border: 'none',
+        width: '100%', 
+        borderRadius: 8
+    }  
+    const textoDoRotulo = 'Nome'
+    const obterTextoDoBotao = () => "Enviar"
+    return ( 
+        <div style={{margin: 'auto', width: 768, backgroundColor: 
+        '#EEE', padding: 12, borderRadius: 8}}>
+            <label className="rotulo" htmlFor="nome" style={{display: 'block', marginBottom: 4}}>
+            {textoDoRotulo}</label>
+            <input Id="nome" type="text" style={{paddingTop: 8, paddingBottom: 8,
+            borderStyle:'hidden', width: '100%', outline: 'none'}}></input>
+            <button style={estilosBotao}>{obterTextoDoBotao()}</button>
+        </div>
+    )
 }   
 
 ReactDOM.render(
